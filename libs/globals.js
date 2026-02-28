77cd186bb91e5e879877bc42df3f61095b2bb38e
otavioguartieri <otavioguartieri@gmail.com>
2026-02-28 00:34:16 -0300

    atualização nos objetos da function modal

--- libs/globals.js
+++ libs/globals.js
@@ -69,15 +69,20 @@ function setNavPosition(act){
     }
 }
 function createTask(title, reward, daily, from, id){
-    if(title.length < 1 || reward.length < 1) return modal(false, 'Por favor, preencha todos os campos.',{close:true, center:true});
+    if(title.length < 1 || reward.length < 1) return modal(false, 'Por favor, preencha todos os campos.',{center:true});
     if(from == 'modal'){
         $('#modalOverlay' + id).remove();
         $('#modal' + id).remove();
     }
 }
-function modal(act, content, confirm, cancel){
-    if (cancel == undefined) cancel = {text:'Cancelar', close: true, center: false};
-    if (confirm == undefined) confirm = {text:'Confirmar', close: true, center: false};
+function modal(act, content, confirm = {}, cancel = {}) {
+
+    const defaultCancel = { text: 'Cancelar', action: '', close: true, center: false };
+    const defaultConfirm = { text: 'Confirmar', action: '', close: true, center: false };
+
+    cancel = { ...defaultCancel, ...cancel };
+    confirm = { ...defaultConfirm, ...confirm };
+
     let actualTime = new Date().getTime();
     $('body').append(`
         <div style="z-index: ${actualTime};" id="modalOverlay${actualTime}" class="modalOverlay"></div>
@@ -85,14 +90,19 @@ function modal(act, content, confirm, cancel){
             <div class="modalContent">
                 <div class="modalInfo">${content}</div>
                 <div class="actions">
-                    <button class="cancel" onclick="${cancel['action'] || ''}$('#modalOverlay${actualTime}').remove(); $('#modal${actualTime}').remove();">${cancel['text'] || 'Cancelar'}</button>
-                    <button class="confirm" onclick="${confirm['action'] || ''}">${confirm['text'] || 'Confirmar'}</button>
+                    <button class="cancel" onclick="${cancel['action']}$('#modalOverlay${actualTime}').remove(); $('#modal${actualTime}').remove();">${cancel['text']}</button>
+                    <button class="confirm" onclick="${confirm['action']}">${confirm['text']}</button>
                 </div>
             </div>
         </div>
     `);
-    if(confirm['close'] == true){
-        $(`#modal${actualTime} .modalContent .actions button.confirm`).attr('onclick', `$('#modalOverlay${actualTime}').remove(); $('#modal${actualTime}').remove();`);
+
+    if(act[0] != 'task' && confirm['close'] == true){
+        $(`#modal${actualTime} .modalContent .actions button.confirm`)
+            .on('click', function () {
+                $(`#modalOverlay${actualTime}`).remove();
+                $(`#modal${actualTime}`).remove();
+            });
     }
     if(confirm['center'] == true){
         $(`#modal${actualTime} .actions`).css({
@@ -100,8 +110,13 @@ function modal(act, content, confirm, cancel){
         });
         $(`#modal${actualTime} .modalContent .actions button.cancel`).remove();
     }
+
     if(cancel['close'] == true){
-        $(`#modal${actualTime} .modalContent .actions button.cancel`).attr('onclick', `$('#modalOverlay${actualTime}').remove(); $('#modal${actualTime}').remove();`);
+        $(`#modal${actualTime} .modalContent .actions button.cancel`)
+            .on('click', function () {
+                $(`#modalOverlay${actualTime}`).remove();
+                $(`#modal${actualTime}`).remove();
+            });
     }
     if(cancel['center'] == true){
         $(`#modal${actualTime} .actions`).css({
@@ -109,6 +124,7 @@ function modal(act, content, confirm, cancel){
         });
         $(`#modal${actualTime} .modalContent .actions button.confirm`).remove();
     }
+
     if(act[0] == 'redeem'){
         $(`#modal${actualTime} .modalInfo`).html(`
             Resgatar <div class="redeemableMessage">${act[1]}</div> por <div class="redeemableTotal"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-bolt"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 2l.018 .001l.016 .001l.083 .005l.011 .002h.011l.038 .009l.052 .008l.016 .006l.011 .001l.029 .011l.052 .014l.019 .009l.015 .004l.028 .014l.04 .017l.021 .012l.022 .01l.023 .015l.031 .017l.034 .024l.018 .011l.013 .012l.024 .017l.038 .034l.022 .017l.008 .01l.014 .012l.036 .041l.026 .027l.006 .009c.12 .147 .196 .322 .218 .513l.001 .012l.002 .041l.004 .064v6h5a1 1 0 0 1 .868 1.497l-.06 .091l-8 11c-.568 .783 -1.808 .38 -1.808 -.588v-6h-5a1 1 0 0 1 -.868 -1.497l.06 -.091l8 -11l.01 -.013l.018 -.024l.033 -.038l.018 -.022l.009 -.008l.013 -.014l.04 -.036l.028 -.026l.008 -.006a1 1 0 0 1 .402 -.199l.011 -.001l.027 -.005l.074 -.013l.011 -.001l.041 -.002z" /></svg>${act[2]}</div>?
